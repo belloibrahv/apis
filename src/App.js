fe3ef2cc911937598902e9bac74fc26b9a62cec4
belloibrahv <belloibrahv@gmail.com>
2022-05-20 05:50:16 -0700

    Sending an http request to rest api to fetch user data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import './App.css'
 
-function App() {
+const App = () => {
+  
+  const [datas, setData] = useState('')
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users?page=2')
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+        throw response
+      })
+      .then(data => {
+        setData(data)
+      })
+      .catch(err => {
+        console.error('Error Fetching Data: ', err)
+        setError(err)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) return 'Loading...'
+  if (error) return 'Error'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello There</h1>
+      <div className="cards">
+        {/* <p key={datas.data.name}>{datas.data.name}</p> */}
+        {datas.data.map((user) => {
+        return (
+          <div className="card">
+            <img key={user.avatar} src={user.avatar} alt='xdmnjdbhdb' />
+            <div>
+              <span key={user.first_name}>{user.first_name}</span>
+              
+              <span key={user.last_name}>{user.last_name}</span>
+            </div>
+            <div key={user.email}>{user.email}</div>
+          </div>
+        );
+      })}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
